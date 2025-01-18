54f29c540c63491898cf7b104a74d09fe31bbd34
Krzysztof <krzysztof@engineer.com>
2025-01-18 17:55:44 +0100

    add static file server

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"go-template/pages"
+	"go-template/static"
 	"log"
 	"log/slog"
 	"net/http"
@@ -19,12 +20,15 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(noCacheMiddleware)
 
 	r.Group(func(r chi.Router) {
 		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			pages.Home().Render(r.Context(), w)
 		})
 	})
+
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServerFS(static.FS)))
 
 	s := http.Server{
 		Addr:    ":4000",
---
+++ middleware.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func noCacheMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Accept") == "text/html" {
+			w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+			w.Header().Set("Pragma", "no-cache")
+			w.Header().Set("Expires", "0")
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
