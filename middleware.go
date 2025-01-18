ddc5c238ce8e3da45b7cd824b0f6f7d790880453
Krzysztof <krzysztof@engineer.com>
2025-01-18 17:57:32 +0100

    fix: no cache middleware

--- middleware.go
+++ middleware.go
@@ -1,10 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func noCacheMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Accept") == "text/html" {
+		if strings.Contains(r.Header.Get("Accept"), "text/html") {
 			w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 			w.Header().Set("Pragma", "no-cache")
 			w.Header().Set("Expires", "0")
